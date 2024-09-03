64cf7b7ab79075c27959adc6455949e56c715edd
colindooley11 <colin_dooley@yahoo.co.uk>
2024-09-04 00:32:10 +0100

    Commit 3 - Continue adding tests on branches of logic at the same depth to get coverage - TODO - persistence is not checked but this will be

--- ClearBank.DeveloperTest.Tests/Services/PaymentServiceTests.cs
+++ ClearBank.DeveloperTest.Tests/Services/PaymentServiceTests.cs
@@ -18,16 +18,38 @@ public class PaymentServiceTests
     [Fact]
     public void Given_A_Bacs_Payment_Request_When_Making_A_Payment_Then_The_Payment_Is_Successful()
     {
-        var paymentService = new TestablePaymentService();
+        var paymentService = new TestablePaymentService(new Account
+        {
+            AllowedPaymentSchemes = AllowedPaymentSchemes.Bacs
+        });
         var result = paymentService.MakePayment(new MakePaymentRequest() { PaymentScheme = PaymentScheme.Bacs});
         Assert.True(result.Success);
     }
-
-    public class TestablePaymentService : PaymentService
+    
+    [Fact]
+    public void Given_A_Faster_Payments_Payment_Request_When_Making_A_Payment_Then_The_Payment_Is_Successful()
     {
+        var paymentService = new TestablePaymentService(new Account
+        {
+            AllowedPaymentSchemes = AllowedPaymentSchemes.FasterPayments,
+            Balance = 1000M
+        });
+        var result = paymentService.MakePayment(new MakePaymentRequest() { PaymentScheme = PaymentScheme.FasterPayments, Amount = 500M});
+        Assert.True(result.Success);
+    }
+
+    private class TestablePaymentService : PaymentService
+    {
+        private readonly Account _account;
+
+        public TestablePaymentService(Account account)
+        {
+            _account = account;
+        }
+
         protected override Account GetAccount(MakePaymentRequest request, AccountDataStore accountDataStore)
         {
-            return new Account() { AllowedPaymentSchemes = AllowedPaymentSchemes.Bacs };
+            return _account;
         }
     }
     
